7b8a38d5280abbb77b9157f249b2999eca0c9a3a
Kriska939 <cristinacorrado93@gmail.com>
2021-09-01 15:30:36 +0200

    added print of 1 email

--- js/script.js
+++ js/script.js
@@ -3,8 +3,14 @@ Vue.config.devtools = true;
 const app = new Vue({
     el: '#app',
     data: {
-
+        emailAddress: "",
     },
     methods: {},
-
+    created() {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((r) => {
+                const response = r.data.response;
+                this.emailAddress = response;
+            });
+    }
 });
