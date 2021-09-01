47775d0a81dccaa5b161b953662ba38ad781b03d
Kriska939 <cristinacorrado93@gmail.com>
2021-09-01 15:43:06 +0200

    added print of 10 emails (no formatting)

--- js/script.js
+++ js/script.js
@@ -8,10 +8,12 @@ const app = new Vue({
     },
     methods: {},
     created() {
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then((r) => {
-                const response = r.data.response;
-                this.emailAddress.push(response);
-            });
+        for (let i = 0; i < this.nEmail; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((r) => {
+                    const response = r.data.response;
+                    this.emailAddress.push(response);
+                });
+        }
     }
 });
