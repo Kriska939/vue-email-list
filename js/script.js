30f7689f1d3f74886b3d79e91fe28af5b0e36dd1
Kriska939 <cristinacorrado93@gmail.com>
2021-09-01 15:41:01 +0200

    changed emailAddress in array

--- js/script.js
+++ js/script.js
@@ -3,14 +3,15 @@ Vue.config.devtools = true;
 const app = new Vue({
     el: '#app',
     data: {
-        emailAddress: "",
+        emailAddress: [],
+        nEmail: 10,
     },
     methods: {},
     created() {
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((r) => {
                 const response = r.data.response;
-                this.emailAddress = response;
+                this.emailAddress.push(response);
             });
     }
 });
